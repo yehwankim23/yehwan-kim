a39e2eee73aecc9e8a3780bb7a8ad53676aa5b91
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2025-01-03 18:37:09 +0900

    Add link to Decimal Age

--- scripts/script.js
+++ scripts/script.js
@@ -33,7 +33,9 @@ async function main() {
   });
 
   if (new Date().getMonth() === 1 && new Date().getDate() === 3) {
-    document.querySelector("#title").innerText = "yehwan.kim 🎉🥳";
+    document.querySelector(
+      "#title"
+    ).innerHTML = `<a href="https://decimal-age.pages.dev" target="_blank">yehwan.kim 🎉🥳</a>`;
   }
 
   const firestore = getFirestore(firebaseApp);
